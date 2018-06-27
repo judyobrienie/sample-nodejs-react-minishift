f26ce326633f31e6cf69ad6fcfc8f522d638ffed
Judy OBrien <judyobrienie@gmail.com>
2018-06-27 14:55:53 +0100

    🎨 Improve structure / format of the code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
       	  <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Judys React</h1>
+          <h1 className="App-title">Welcome to Students React</h1>
         </header>
         <p className="App-intro">
 	  Welome to Your React Project.  
