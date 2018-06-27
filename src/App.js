5d2cc7f0fff241af512c37ad297e06f008810303
Judy OBrien <judyobrienie@gmail.com>
2018-06-27 12:54:09 +0100

    test

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
 	  Welome to Your React Project.  
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    </p>
       </div>
     );
   }
