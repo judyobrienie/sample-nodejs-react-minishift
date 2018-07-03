e9c9c85ba9a226e311e2d6d1317b7cf490bd5f6f
Judy OBrien <judyobrienie@gmail.com>
2018-07-03 17:37:10 +0100

    🚀 testing

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
       	  <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Your React Project</h1>
+          <h1 className="App-title">Welcome to Judys React Project</h1>
         </header>
         <p className="App-intro">
-	  Welome to Your React Project. Good Luck!
+	  Good Luck!
           To get started, edit <code>src/App.js</code> and save to reload.
     </p>
       </div>
